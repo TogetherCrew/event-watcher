83b8441935d409aada47dc3869fc06be41865c00
Cyrille Derche <c.derche@me.com>
2024-11-01 13:33:42 +0100

    add sepolia contract

--- src/watchables.ts
+++ src/watchables.ts
@@ -1,5 +1,5 @@
 import { AbiEvent, Address, Chain, parseAbiItem } from 'viem';
-import { mainnet } from 'viem/chains';
+import { sepolia } from 'viem/chains';
 
 type Watchable = {
   chain: Chain;
@@ -13,15 +13,26 @@ type Watchable = {
 
 export const watchables: Watchable[] = [
   // Example: Watch WETH Transfer (Ethereum)
+  // {
+  //   chain: mainnet,
+  //   address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  //   event: parseAbiItem(
+  //     'event Transfer(address indexed from, address indexed to, uint256 value)',
+  //   ),
+  //   target: {
+  //     queue: 'TestQueue',
+  //     name: 'TestEvent',
+  //   },
+  // },
   {
-    chain: mainnet,
-    address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    chain: sepolia,
+    address: '0x8ff1dd3967a87c1eb46bd60b2bbf9d7eaa987c1b',
     event: parseAbiItem(
-      'event Transfer(address indexed from, address indexed to, uint256 value)',
+      'event Issue(address indexed account, uint indexed tokenId)',
     ),
     target: {
-      queue: 'TestQueue',
-      name: 'TestEvent',
+      queue: 'API_QUEUE',
+      name: 'EngagementTokenIssued',
     },
   },
 ];
